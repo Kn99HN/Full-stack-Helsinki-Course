a3cf48a3d4377b102a219ea39454af51b13ce069
[Kn99HN] <khanhnguyen99hn@gmail.com>
2020-01-20 23:48:19 -0500

    update

--- part2a/src/App.js
+++ part2a/src/App.js
@@ -4,7 +4,7 @@ import Note from './components/Note'
 const App = (props) => {
   const [showAll, setShowAll] = useState(true)
   const [notes, setNotes] = useState(props.notes)
-  
+
   //if showall is false, we filter the important one
   const notesToShow = showAll
                 ? notes
