6c3d21908e6ae7f446dd7660b2b97377f5243dab
Kn99HN <khanhnguyen99hn@gmail.com>
2020-05-08 21:50:34 -0400

    finishing lecture 3 and first hw

---
+++ part3-exercise/index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+
+let phones = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello world </h1>")
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(phones)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = phones.find(person => person.id === id)
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    const length = phones.length
+    const date = new Date()
+    res.send(
+        `<div>There are currently ${length} entries in the phonebook</div>
+        <br/>
+        <div> ${date} </div>`
+    )
+})
+
+const generateID =  () => {
+    return Math.floor((Math.random() * 1000) + phones.length + 1)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if(!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'missing values'
+        })
+    }
+    
+    if(phones.find(phone => phone.name === body.name)) {
+        return res.status(400).json({
+            error: 'duplicate values'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID()
+    }
+
+    phones = phones.concat(person)
+    res.json(phones)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    phones = phones.filter(phone => phone.id !== id)
+    res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`)
+})
---
+++ part3/NOTES.md
@@ -0,0 +1,10 @@
+# package.json
+
+Similar to gradle build tools, package.json allows us to define
+the entry points of the project and simple command to easily run
+our project
+
+nodemon help us track changes in development
+
+--save-dev is to saved to dev dependency, which is needed ONLY 
+during development
